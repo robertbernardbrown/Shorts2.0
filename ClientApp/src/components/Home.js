67753405f5d33315e8f6b145d68d66908705db5b
Bob Brown <robertbernardbrown@gmail.com>
2018-07-01 12:12:42 -0400

    sets up form and button for shortist

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -3,23 +3,55 @@ import React, { Component } from 'react';
 export class Home extends Component {
   displayName = Home.name
 
+  state = {
+    zipcode: "",
+  }
+
+  searchZip = () => {
+    console.log(this.state.zipcode)
+  }
+
+  fillForm = (e) => {
+    this.setState({
+      zipcode: e.target.value
+    })
+    console.log(this.state.zipcode);
+  }
+
   render() {
+
     return (
       <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we've also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
+        <h1>The Shortist</h1>
+        <h2>Should you wear shorts or nah?</h2>
+
+        <p>
+          We'll keep this short:
+          <ul>
+            <li>Enter your zipcode</li>
+            <li>Find out if it's cool to wear shorts today</li>
+            <li>Chillax</li>
+          </ul>
+        </p>
+
+        <form method= "post">
+          <div id="zipform">
+            <input type="text" id="zipcode" value={this.state.fillForm} onChange={this.fillForm}/>
+          </div>
+          <button className="btn btn-primary button" id="setzipcode" onClick={this.searchZip}>
+            Set Zipcode
+          </button>
+        </form>
+
+        <p>
+          <div id="display">
+          </div>
+        </p>
+
+        <p>
+          <div id="display-reco">
+          </div>
+        </p>
       </div>
     );
   }
--- ClientApp/src/components/Layout.js
+++ ClientApp/src/components/Layout.js
@@ -9,10 +9,10 @@ export class Layout extends Component {
     return (
       <Grid fluid>
         <Row>
-          <Col sm={3}>
+          {/* <Col sm={3}>
             <NavMenu />
-          </Col>
-          <Col sm={9}>
+          </Col> */}
+          <Col sm={12}>
             {this.props.children}
           </Col>
         </Row>

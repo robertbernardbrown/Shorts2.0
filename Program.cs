23cf697f5937b1ec30227cc72524236aae0a59dd
Bob Brown <robertbernardbrown@gmail.com>
2018-07-01 11:17:53 -0400

    adds template files to play with

--- Program.cs
+++ Program.cs
@@ -1,48 +1,24 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
-namespace list_quickstart
+namespace Shorts2._0
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            WorkingWithStrings();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static void WorkingWithStrings()
-        {
-            var names = new List<string> { "<name>", "Ana", "Felipe" };
-            foreach (var name in names)
-            {
-                Console.WriteLine($"Hello {name.ToUpper()}!");
-            }
-
-            Console.WriteLine();
-            names.Add("Maria");
-            names.Add("Bill");
-            names.Remove("Ana");
-            foreach (var name in names)
-            {
-                Console.WriteLine($"Hello {name.ToUpper()}!");
-            }
-
-            Console.WriteLine($"My name is {names[0]}");
-            Console.WriteLine($"I've added {names[2]} and {names[3]} to the list");
-
-            Console.WriteLine($"The list has {names.Count} people in it");
-
-            var index = names.IndexOf("Felipe");
-            Console.WriteLine($"The name {names[index]} is at index {index}");
-
-            var notFound = names.IndexOf("Not Found");
-            Console.WriteLine($"When an item is not found, IndexOf returns {notFound}");
-
-            names.Sort();
-            foreach (var name in names)
-            {
-                Console.WriteLine($"Hello {name.ToUpper()}!");
-            }
-        }
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
     }
 }

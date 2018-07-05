7e3ad3f08c924475899bede0f269826da41ba681
Bob Brown <robertbernardbrown@gmail.com>
2018-07-05 17:03:40 -0400

    new builds and tightened up front end styling and removed unneeded code

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -44,56 +44,62 @@ export class Home extends Component {
           <Panel.Body>Panel content</Panel.Body>
         </Panel> */}
 
-        <Panel>
-          <div className="card">
-            <div className="container">
-              <p>
-                We'll keep this short:
-                <ul>
-                  <li>Enter your zipcode</li>
-                  <li>Find out if it's cool to wear shorts today</li>
-                  <li>Chillax</li>
-                </ul>
-              </p>
-            </div>
+        <div className="container">
+          <Panel>
+            <div className="card">
+              <div className="container">
+                <p>
+                  We'll keep this short:
+                  <ul>
+                    <li>Enter your zipcode</li>
+                    <li>Find out if it's cool to wear shorts today</li>
+                    <li>Chillax</li>
+                  </ul>
+                </p>
+              </div>
 
-            <div className="container">
-              <form method= "post">
-                <div id="zipform">
-                  <input type="text" id="zipcode" value={this.state.fillForm} onChange={this.fillForm}/>
-                </div>
-                <button className="btn btn-block btn-primary button" id="setzipcode" onClick={this.searchZip}>
-                  Set Zipcode
-                </button>
-              </form>
+              <div className="formContainer">
+                <form method= "post">
+                  <div id="zipform">
+                    <input type="text" id="zipcode" value={this.state.fillForm} onChange={this.fillForm}/>
+                  </div>
+                  <button className="btn btn-block btn-primary button" id="setzipcode" onClick={this.searchZip}>
+                    Set Zipcode
+                  </button>
+                </form>
+              </div>
             </div>
-          </div>
-        </Panel>
+          </Panel>
+        </div>
 
         {!this.state.searchComplete ? 
         <div id="display">
           <p></p>
         </div>
         :
-        <Panel>
-          <div>
-            <div id="weather-display">
-              <p>You're currently in {this.state.forecasts.location.name}, {this.state.forecasts.location.region}.</p>
-              <p>It's currently {this.state.forecasts.current.temp_f} degrees and {this.state.forecasts.current.condition.text} out.</p>
-            </div>
-            <div id="reco-display">
-              {this.state.forecasts.current.temp_f > 70 ? 
-              <div>
-                <p>Wear your shorts!</p>
+        <div className="container">
+          <Panel>
+            <div>
+              <div id="weather-display">
+                <p>You're currently in {this.state.forecasts.location.name}, {this.state.forecasts.location.region}.</p>
+                <p>It's currently {this.state.forecasts.current.temp_f} degrees and {this.state.forecasts.current.condition.text} out.</p>
               </div>
-              :
-              <div>
-                <p>No shorts today :(</p>
+              <div id="reco-display">
+                {this.state.forecasts.current.temp_f > 70 ? 
+                <div>
+                  <i class="fas fa-sun fa-5x"></i>
+                  <p>Wear your shorts!</p>
+                </div>
+                :
+                <div>
+                  <i class="fas fa-hand-paper fa-5x"></i>
+                  <p>No shorts today :(</p>
+                </div>
+                }
               </div>
-              }
             </div>
-          </div>
-        </Panel>
+          </Panel>
+        </div>
         }
       </div>
     );
